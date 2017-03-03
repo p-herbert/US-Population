dc20cc490ec174b39b33fc55c6422e48cce42748
p-herbert <pete.a.herbert@gmail.com>
2017-03-03 13:15:42 -0800

    (feat) Add method sumTotal

--- app.js
+++ app.js
@@ -1,6 +1,14 @@
 $(function() {
   const url = "https://api.census.gov/data/2015/pep/population?get=POP,GEONAME&for=state";
 
+  function sumTotal(states) {
+    let total = 0;
+
+    states.forEach(state => (total += parseInt(state[0], 10)));
+
+    return total;
+  }
+
   $.ajax({
     type: 'GET',
     url: url,
